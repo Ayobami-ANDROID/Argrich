53a8da9ffcc6a52852f37a2a56217167723defb4
himarnoel <olaimarnoel>
2024-07-10 08:35:36 +0100

    working on signup

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Signup = () => {
+  return <div className="min-h-screen bg-[#F5F5F5] grid lg:grid-cols-2  p-4">
+    <div className="flex   bg-[#D9D9D9] rounded-[30px]"></div>
+    <div className="flex "></div>
+  </div>;
+};
+
+export default Signup;
