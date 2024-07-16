04e69635b2c870e51449bbfd376c17afb81e4878
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-07-16 14:38:44 +0100

    changes

---
+++ src/components/TopSelling.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopSelling = () => {
+  return (
+    <div className='my-8'>
+         <h1 className="my-4 font-[600] text-[28px]">Top Selling Items</h1>
+    </div>
+  )
+}
+
+export default TopSelling
