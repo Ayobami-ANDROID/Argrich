49bb30e3625be0be0b1991dd57a7fdc98e31d142
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-08-12 23:48:42 +0100

    messages

--- src/components/TodayDeals.jsx
+++ src/components/TodayDeals.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Item from "./Item";
 import { useDispatch, useSelector } from "react-redux";
 import Skeleton from "react-loading-skeleton";
+import { getProducts,productReset } from "../features/product/productSlice";
 
 const TodayDeals = () => {
   const dispatch = useDispatch();
