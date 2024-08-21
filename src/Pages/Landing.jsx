31821ce61417649b3c1edc73eb87ae0179280678
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-08-21 13:35:00 +0100

    messages

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/LandingComponent.jsx/Header'
+
+const Landing = () => {
+  return (
+    <div className='flex flex-col'>
+        <div className="bg-url[('')]">
+            <div className='flex items-center justify-center'>
+            <Header/>
+            </div>
+       
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Landing
