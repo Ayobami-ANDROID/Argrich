fd3ea44f7d4a1fb5cf680e4068991abb955df4ef
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-22 08:27:44 +0100

    indenting adjustment

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import Header from '../components/LandingComponent.jsx/Header'
+import React from "react";
+import Header from "../components/LandingComponent.jsx/Header";
 
 const Landing = () => {
   return (
-    <div className='flex flex-col'>
-        <div className="bg-url[('')]">
-            <div className='flex items-center justify-center'>
-            <Header/>
-            </div>
-       
+    <div className="flex flex-col">
+      <div className="bg-url[('')]">
+        <div className="flex items-center justify-center">
+          <Header />
         </div>
-        
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
