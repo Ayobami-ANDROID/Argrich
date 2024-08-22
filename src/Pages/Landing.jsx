e34ade265b954424e4690fba7581fece1d9bb71d
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-22 13:39:57 +0100

    adjusted indentation

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,49 +3,47 @@ import Hero from "../components/LandingComponent.jsx/Hero";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const Landing = () => {
+  useGSAP(() => {
+    gsap.from("#test", {
+      x: "-100%",
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: "#test",
+        start: "top bottom",
+        end: "bottom top",
+        // markers: true,
+        toggleActions: "play pause reverse pause",
+      },
+      ease: "bounce.out",
+    });
+  }, []);
 
-    useGSAP(() => {
-
-        gsap.from('#test', {
-            x: '-100%',
-            opacity: 0,
-            duration: 2,
-            scrollTrigger: {
-                trigger: '#test',
-                start: 'top bottom',
-                end: 'bottom top',
-                // markers: true,
-                toggleActions: "play pause reverse pause"
-            },
-            ease: 'bounce.out'
-
-
-        })
-
-    }, [])
-
-
-
-    return (
-        <div className="">
-            <Hero />
-            <div className="flex flex-col mt-10 mb-10 gap-y-7 
-      " id="box">
-                {" "}
-                <p className="text-center text-[#0F4400] font-semibold font-manrope text-[34px] " id="test">
-                    Our farm is a thriving five-hectare oasis <br /> located in Kuje, Abuja.
-                </p>
-                <button className="mx-auto px-[20px] py-[12px] shadow-[0px_2px_12.8px_0px_rgba(0,0,0,0.25)] bg-[#0F4400] rounded-lg text-white font-manrope  font-semibold text-base">
-                    Direction
-                </button>
-            </div>
-
-        </div>
-
-    );
+  return (
+    <div className="">
+      <Hero />
+      <div
+        className="flex flex-col mt-10 mb-10 gap-y-7 
+      "
+        id="box"
+      >
+        {" "}
+        <p
+          className="text-center text-[#0F4400] font-semibold font-manrope text-[34px] "
+          id="test"
+        >
+          Our farm is a thriving five-hectare oasis <br /> located in Kuje,
+          Abuja.
+        </p>
+        <button className="mx-auto px-[20px] py-[12px] shadow-[0px_2px_12.8px_0px_rgba(0,0,0,0.25)] bg-[#0F4400] rounded-lg text-white font-manrope  font-semibold text-base">
+          Direction
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
