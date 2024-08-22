20d9eedcc17a3a7c912587e07c4f863eca682186
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-22 08:26:06 +0100

    added font-manrope  to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [
     // require('daisyui'),
   ],
-  variants:{
-    variants:{
-      extend:{
-        display:["focus-group"]
-      }
-    }
-  }
-}
+  variants: {
+    variants: {
+      extend: {
+        display: ["focus-group"],
+      },
+    },
+  },
+};
