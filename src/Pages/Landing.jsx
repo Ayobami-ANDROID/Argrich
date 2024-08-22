daa72be1466fee50f3e94d69272854725b21e695
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-08-22 11:14:36 +0100

    done with hero section

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -4,26 +4,29 @@ import herobg from "../images/bgheros.png";
 import bag from "../images/bag.svg";
 import texture from "../images/texture.png";
 import heroimg from "../images/heroimg.png";
+import onion from "../images/onion.png";
+import corn from "../images/corn.png";
 
 const Landing = () => {
   return (
     <div className="flex flex-col">
       <div className="px-4">
         <div className="relative">
-          {" "}
-          <div className="  bg-[#FFE6AE] min-h-screen  inline-block w-full  bg-cover  mt-2 rounded-[15px] ">
+          <img src={corn} alt=""  className="absolute -right-[1rem] top-[24rem] "/>
+          <img src={onion} alt=""  className="absolute top-[26.4rem] left-12"/>
+          <div className="  bg-[#FFE6AE]   w-full   mt-2 rounded-[15px] ">
             <img
               src={texture}
               alt=""
-              className=" rounded-[15px]    mix-blend-plus-lighter  object-cotain"
+              className=" rounded-[15px]  min-h-[714px]   mix-blend-plus-lighter  object-cover "
             />
           </div>
-          <div className="absolute top-4 w-full">
+          <div className="absolute top-2 w-full">
             <div className="flex items-center justify-center  w-full ">
               <Header />
             </div>
-            <div className="text-center max-w-[596px] mx-auto space-y-4">
-              <p className="text-[#0F4400] font-semibold text-[64px] font-rubik  mt-6">
+            <div className="text-center max-w-[596px] mx-auto flex flex-col gap-y-4 mt-6">
+              <p className="text-[#0F4400] font-semibold text-[64px] font-rubik  ">
                 Farm to Table <br /> Freshness.
               </p>
               <p className="text-[#4F584C] font-manrope font-medium text-[18px]">
@@ -36,7 +39,7 @@ const Landing = () => {
               </button>
             </div>
           </div>
-          <div className="absolute -bottom-72 mx-auto left-1/2  transform  -translate-x-1/2 px-10 w-full">
+          <div className="absolute -bottom-[14rem] mx-auto left-1/2  transform  -translate-x-1/2 px-[8rem] w-full">
             {" "}
             <img
               src={heroimg}
